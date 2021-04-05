15d5a5f0bde97f88ee436d6dc8e86ce8414e5b1c
raninab <75264045+raninab@users.noreply.github.com>
2021-04-05 20:59:17 +0300

    Add files via upload

---
+++ logout.py
@@ -0,0 +1,7 @@
+#exit
+def Exit():
+    result = tkMessageBox.askquestion('System', 'Are you sure you want exit ?', icon="warning")
+    if result == 'yes':
+       app.destroy()
+       exit()
+#Exit()
