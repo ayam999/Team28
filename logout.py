d6fe4d80e45bffc58cbd0c6d29fdd9a7b4d45eee
khawlal <khawlal@ac.sce.ac.il>
2021-05-06 12:11:25 +0300

    khawla updeted

--- logout.py
+++
@@ -1,7 +0,0 @@
-#exit
-def Exit():
-    result = tkMessageBox.askquestion('System', 'Are you sure you want exit ?', icon="warning")
-    if result == 'yes':
-       app.destroy()
-       exit()
-#Exit()
