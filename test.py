3b2c79e811ae97cfa90a45b8f4a81f6b7278b73d
khawlal <khawlal@ac.sce.ac.il>
2021-05-06 17:41:12 +0300

    test

---
+++ test.py
@@ -0,0 +1,55 @@
+import unittest
+import pyrebase
+import App
+from App import delete_info_item 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+Config = {
+  apiKey: "AIzaSyDj83l21N_0vFiJP2_RhiUTN2qr8X84dfI",
+  authDomain: "flaskdb-fb78d.firebaseapp.com",
+  databaseURL: "https://flaskdb-fb78d-default-rtdb.firebaseio.com",
+  projectId: "flaskdb-fb78d",
+  storageBucket: "flaskdb-fb78d.appspot.com",
+  messagingSenderId: "291466022293",
+  appId: "1:291466022293:web:462cf3b91963df70b87a9c",
+  measurementId: "G-W08M46DJ3R"
+}
+
+
+db = firestore.client()
+
+firebase = pyrebase.initialize_app(config)
+auth= firebase.auth()
+
+class TestHello(unittest.TestCase):
+    def test_registerAdmine(self):
+     try:
+         email="bushra@gmail.com"
+         firstname="bushra"
+         id="123456789"
+         lastname="alh"
+         password="12345678" 
+         data={"email":email,"firstname":firstname,"id":id,"lastname":lastname,"password":password}
+         db.collection(u'Adminetabel').document().set(data)
+         self.assertTrue(True)
+     except:
+            self.assertTrue(False)
+
+    #Register User with uncorrect details
+    def test_registerAdmine_uncorrect(self):
+        try:
+    
+            email="bushra"
+            firstname="bu"
+            id="12389"
+            lastname="alh"
+            password="12345678"
+            data={"email":email,"firstname":firstname,"id":id,"lastname":lastname,"password":password}
+            db.collection(u'Adminetabel').document().set(data)
+            self.assertTrue(False)
+        except:
+            self.assertTrue(True)
+
+ 
