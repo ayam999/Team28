ce88c06a9f9c7b88f5bc544d3d478ffa73cf2001
raninab <raninab@ac.sce.ac.il>
2021-05-08 17:18:31 +0300

    raninn

--- forms.py
+++ forms.py
@@ -28,6 +28,40 @@ class SignOutForm(FlaskForm):
     submit = SubmitField('logout')
 
 class update_sprint(FlaskForm):
+        DEFAULT_OPTIONS = {
+        "server": "http://localhost:2990/jira",
+        "auth_url": "/rest/auth/1/session",
+        "context_path": "/",
+        "rest_path": "api",
+        "rest_api_version": "2",
+        "agile_rest_path": GreenHopperResource.GREENHOPPER_REST_PATH,
+        "agile_rest_api_version": "1.0",
+        "verify": True,
+        "resilient": True,
+        "async": False,
+        "async_workers": 5,
+        "client_cert": None,
+        "check_update": False,
+        # amount of seconds to wait for loading a resource after updating it
+        # used to avoid server side caching issues, used to be 4 seconds.
+        "delay_reload": 0,
+        "headers": {
+            "Cache-Control": "no-cache",
+            # 'Accept': 'application/json;charset=UTF-8',  # default for REST
+            "Content-Type": "application/json",  # ;charset=UTF-8',
+            # 'Accept': 'application/json',  # default for REST
+            # 'Pragma': 'no-cache',
+            # 'Expires': 'Thu, 01 Jan 1970 00:00:00 GMT'
+            "X-Atlassian-Token": "no-check",
+        }
+    }
+
+    checked_version = False
+
+    # TODO(ssbarnea): remove these two variables and use the ones defined in resources
+    JIRA_BASE_URL = Resource.JIRA_BASE_URL
+    AGILE_BASE_URL = GreenHopperResource.AGILE_BASE_URL
+
     def update_sprint(self, id, name=None, startDate=None, endDate=None, state=None):
         payload = {}
         if name:
