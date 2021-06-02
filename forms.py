21ff2c70dc06b0f3ed9ec0c030dad15e201ddb07
khawlal <khawlal@ac.sce.ac.il>
2021-06-03 01:00:20 +0300

    1

--- forms.py
+++ forms.py
@@ -1,26 +1,4 @@
-<<<<<<< Updated upstream
-from flask import  Flask,render_template,session,redirect,url_for
-from wtforms import StringField, PasswordField , SubmitField , RadioField,TextAreaField
-from flask_wtf import FlaskForm
-from  wtforms.validators import  DataRequired,Length,EqualTo, Email
-
-class  RigistrationForm(FlaskForm):
-     username=StringField(label='username',validators=[DataRequired(),Length(min=3,max=20)])
-     password=PasswordField(label='password',validators=[DataRequired(),Length(min=6,max=8)])
-     id=StringField(label='id',validators=[DataRequired(),Length(min=9,max=9)])
-     email=StringField(label='email',validators=[DataRequired(),Email()])
-     firstname=StringField(label='firstname',validators=[DataRequired(),Length(min=3,max=20)])
-     lastname=StringField(label='lastname',validators=[DataRequired(),Length(min=3,max=20)])
-     comfirmPassward=PasswordField(label='comfirm Passward',validators=[DataRequired(),EqualTo('passwors')])
-     submit=SubmitField(label='Sign up')
-
-
-
-class LoginForm(FlaskForm):
-    username = StringField(label='username', validators=[DataRequired(), Length(min=3, max=20)])
-    password = PasswordField(label='password', validators=[DataRequired(), Length(min=6, max=8)])
-    submit = SubmitField('Login')
-=======
+#from App import Demand
 from flask import  Flask,render_template,session,redirect,url_for
 from wtforms import StringField, PasswordField , SubmitField , RadioField,TextAreaField
 from flask_wtf import FlaskForm
@@ -40,18 +18,32 @@ class signupForm(FlaskForm):
 
 
 
-class SignOutForm(FlaskForm):
-    submit = SubmitField('logout')
-
 class LoginForm(FlaskForm):
     email=StringField(label='email',validators=[DataRequired(),Email()])
     password = PasswordField(label='password', validators=[DataRequired(), Length(min=6, max=8)])
     submit = SubmitField('Login')
 
+class SignOutForm(FlaskForm):
+    submit = SubmitField('logout')
+'''
 class addDemandForm(FlaskForm):
     email=StringField(label='email',validators=[DataRequired(),Email()])
-    demand = StringField(label='demand',validators=[DataRequired(),Length(min=3,max=100)])
+    password=PasswordField(label='email',validators=[DataRequired(),Email()])
+    demand = TextAreaField(label='demand',validators=[DataRequired()])
     siprintNumber=StringField(label='siprintNumber',validators=[DataRequired(),Length(min=1,max=20)])
     submit = SubmitField('add')
+    '''
 
->>>>>>> Stashed changes
+class addDemandForm(FlaskForm):
+    email = StringField("email", validators=[DataRequired()])
+    Demand= StringField("Demand", validators=[DataRequired()])
+    #demandNumber= StringField("demandNumber", validators=[DataRequired()])
+    submit = SubmitField('add demand')
+
+
+class DeleteDemanForm(FlaskForm):
+    email = StringField("email", validators=[DataRequired()])
+
+    Demand = StringField("Demand", validators=[DataRequired()])
+    submit = SubmitField('Delete Deman')
+   
