d5cab120f61ee25de110641908f73c689ecfb28f
manalab <manalab@ac.sce.ac.il>
2021-06-05 17:26:37 +0300

    select fiueld addition

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 #from App import Demand
 from flask import  Flask,render_template,session,redirect,url_for
-from wtforms import StringField, PasswordField , SubmitField , RadioField,TextAreaField
+from wtforms import StringField, PasswordField , SubmitField , RadioField,TextAreaField,SelectField
 from flask_wtf import FlaskForm
 from wtforms import validators
 from  wtforms.validators import  DataRequired,Length,EqualTo, Email
