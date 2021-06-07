1e4b9a6d4124d5f5b18392142c8e6a3009f8846d
naemaal <75208493+naemaal@users.noreply.github.com>
2021-06-07 16:06:46 +0300

    new test

--- test_pace1.py
+++ test_pace1.py
@@ -1,21 +1,22 @@
 import unittest
 import pyrebase
 import App
-from App import delete_info_item 
+#from App import delete_info_item 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-Config = {
-  apiKey: "AIzaSyDj83l21N_0vFiJP2_RhiUTN2qr8X84dfI",
-  authDomain: "flaskdb-fb78d.firebaseapp.com",
-  databaseURL: "https://flaskdb-fb78d-default-rtdb.firebaseio.com",
-  projectId: "flaskdb-fb78d",
-  storageBucket: "flaskdb-fb78d.appspot.com",
-  messagingSenderId: "291466022293",
-  appId: "1:291466022293:web:462cf3b91963df70b87a9c",
-  measurementId: "G-W08M46DJ3R"
+config={
+  "apiKey": "AIzaSyDj83l21N_0vFiJP2_RhiUTN2qr8X84dfI" ,
+  "authDomain": "flaskdb-fb78d.firebaseapp.com",
+  "databaseURL": "https://flaskdb-fb78d-default-rtdb.firebaseio.com",
+  "projectId": "flaskdb-fb78d",
+  "storageBucket": "flaskdb-fb78d.appspot.com",
+  "messagingSenderId": "291466022293",
+  "appId": "1:291466022293:web:462cf3b91963df70b87a9c",
+  "measurementId": "G-W08M46DJ3R"
 }
+
 
 
 db = firestore.client()
@@ -62,3 +63,10 @@ class TestHello(unittest.TestCase):
          self.assertTrue(True)
      except:
             self.assertTrue(False)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
