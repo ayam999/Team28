9f1857a6cce4c6beb5cd6226dacbc2b11244fd01
naemaal <75208493+naemaal@users.noreply.github.com>
2021-06-09 11:57:27 +0300

    n

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ import App
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-
+from App import app
+import json 
 config={
   "apiKey": "AIzaSyDj83l21N_0vFiJP2_RhiUTN2qr8X84dfI" ,
   "authDomain": "flaskdb-fb78d.firebaseapp.com",
@@ -420,19 +421,107 @@ class TestHello(unittest.TestCase):
         except:
             self.assertTrue(False)
 
+class TestHello(unittest.TestCase):
+    #User login with correct details
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_homePage(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+
+    
+
+    def test_login_logout(self):
+        taster = app.test_client(self)
+        rv = taster.post('/login' , data=dict(email="mor0981@gmail.com",password="123456"),follow_redirects=True)
+        self.assertTrue(rv.status, '200 OK')
+        self.assertFalse('ברוכים'.encode() in rv.data)
+        rv= taster.get('/logout',follow_redirects=True)
+        self.assertFalse('התנתקת בהצלחה'.encode() in rv.data)
+
+    def test_login_session(self):
+        taster = app.test_client(self)
+        rv = taster.post('/login' , data=dict(email="mor0981@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.get('/login',follow_redirects=True)
+        self.assertFalse('ברוכים'.encode() in rv.data)
+        rv= taster.get('/logout',follow_redirects=True)
+
+    def test_delete_user(self):
+        taster = app.test_client(self)
+        rv = taster.post('/register' , data=dict(email="test@gmail.com",password="123456",name="test",last="test"),follow_redirects=True)
+        rv = taster.post('/login' , data=dict(email="test@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.post('/unregister' , data=dict(email="test@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.post('/login' , data=dict(email="test@gmail.com",password="123456"),follow_redirects=True)
+        self.assertFalse('שם משתמש או סיסמא לא נכונים'.encode() in rv.data)
+
+    def test_comment(self):
+        taster = app.test_client(self)
+        rv = taster.post('/register' , data=dict(email="test2@gmail.com",password="123456",name="test",last="test"),follow_redirects=True)
+        rv = taster.post('/login' , data=dict(email="test2@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.post('/comments/פארק%20ליכטנשטיין',data=dict(comment="test"),follow_redirects=True)
+        rv = taster.get('/comments/פארק%20ליכטנשטיין')
+        self.assertFalse('test'.encode() in rv.data)
+        rv = taster.post('/unregister',data=dict(email="test2@gmail.com",password="123456"),follow_redirects=True)
 
 
 
+    def test_login_as_admin(self):
+        taster = app.test_client(self)
+        rv = taster.post('/login' , data=dict(email="mor0981@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.get('/login',follow_redirects=True)
+        rv= taster.get('/logout',follow_redirects=True)
+        self.assertFalse('test'.encode() in rv.data)
+    def test_login_as_visit(self):
+        taster = app.test_client(self)
+        rv = taster.post('/login' , data=dict(email="dani@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.get('/login',follow_redirects=True)
+        self.assertFalse('משתמשים'.encode() in rv.data)
+        rv= taster.get('/logout',follow_redirects=True)
 
-class intgrationtest(unittest.TestCase):
-    def test_show_tutor(self):
-        taster = app.test_client.get(form("show"))
-        self.assertEquals(taster.status, 200)
-        self.assertTemplateUsed(taster,"app/show.html")
+    
+    def test_add_admin(self):
+        taster = app.test_client(self)
+        rv = taster.post('/login' , data=dict(email="mor0981@gmail.com",password="123456"),follow_redirects=True)
+        rv = taster.get('/login',follow_redirects=True)
+        rv = taster.post('/registerByAdmin' , data=dict(name="טסט",last="טסט",email="test3@gmail.com",password="123456",Admin="true"),follow_redirects=True)
+        rv= taster.get('/logout',follow_redirects=True)
+        rv = taster.post('/login' , data=dict(email="test3@gmail.com",password="123456"),follow_redirects=True)
+        self.assertFalse('ברוכים'.encode() in rv.data)
+        rv = taster.post('/unregister',data=dict(email="test3@gmail.com",password="123456"),follow_redirects=True)
 
-        taster = self.client.get(form('studentsignup'))
-        self.assertEquals(taster.status, 200)
-        self.assertTemplateUsed(taster,"app/studentsignup.html")
+        
+
+    # def delet_comment(self):
+    #     taster = app.test_client(self)
+    #     rv = taster.post('/register' , data=dict(email="test3@gmail.com",password="123456",name="test",last="test"),follow_redirects=True)
+    #     rv = taster.post('/login' , data=dict(email="test3@gmail.com",password="123456"),follow_redirects=True)
+    #     rv = taster.post('/comments/פארק%20ליכטנשטיין',data=dict(comment="test"),follow_redirects=True)
+    #     rv = taster.get('/comments/פארק%20ליכטנשטיין')
+    #     self.assertTrue('test'.encode() in rv.data)
+
+    
+        
+
+         
+        
+    # def test_correct(self):
+    #     try:
+    #         auth.sign_in_with_email_and_password("mor0981@gmail.com","123456")
+    #         self.assertTrue(True)
+    #     except:
+    #         self.assertTrue(False)
+    # #User login with incorrect details
+    
+    # def test_incorrect(self):
+    #     try:
+    #         auth.sign_in_with_email_and_password("mor081@gmail.com","12661266")
+    #         self.assertTrue(False)
+    #     except:
+    #         self.assertTrue(True)
+    
+
 
 
 
